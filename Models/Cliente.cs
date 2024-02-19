04b53aa7438b8e3af7131d88d71bec9948cf4c9c
PC01-1H <PC01-1H@PC10-1H>
2024-02-19 12:46:23 -0600

    Agregar archivos de proyecto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConexionCris.Models
+{
+    [Table("clientes")]
+    public class Cliente
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ Models/Veterinario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConexionCris.Models
+{
+    [Table("veterinario")]
+    public class Veterinario
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Edad {  get; set; }
+
+        public int Telefono { get; set; }
+
+    }
+}
