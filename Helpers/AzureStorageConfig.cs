6d06979967e25891d8dde0559b8efcaa6130205b
Cristian6663 <deltasoceralto@gmail.com>
2024-02-28 22:08:21 -0600

    cambio

---
+++ Helpers/AzureStorageConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace ConexionCris.Helpers
+{
+    public class AzureStorageConfig
+    {
+        public string Cuenta { get; set; }
+        public string Llave { get; set; }
+        public string Contenedor { get; set; }
+    }
+}
---
+++ Migrations/20240304180531_version3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConexionCris.Migrations
+{
+    /// <inheritdoc />
+    public partial class version3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumCita = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Foto = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "veterinario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Telefono = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_veterinario", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.DropTable(
+                name: "veterinario");
+        }
+    }
+}
